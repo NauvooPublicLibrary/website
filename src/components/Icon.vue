567480d9133c2046129038c819d8afa73a1c9c5c
speegs92 <christian@levesque.dev>
2026-01-13 12:52:59 -0600

    added <Icon> component for FontAwesome icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+﻿<script
+	lang='ts'
+	setup
+>
+type Props = {
+	family: 'solid'|'brands'
+	icon: string;
+}
+
+const { family = 'solid' } = defineProps<Props>();
+</script>
+
+<template>
+<i :class='`fa${family[0]} fa-${icon}`'/>
+</template>
