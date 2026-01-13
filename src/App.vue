0040c271448611c552e17121aaa9c6f8ec563f32
speegs92 <christian@levesque.dev>
2026-01-13 14:00:35 -0600

    added <ContactBanner> to the app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
-<template>
+<script
+	lang='ts'
+	setup
+>
+import ContactBanner from '@/layout/ContactBanner.vue';
+</script>
 
+<template>
+<div>
+	<ContactBanner/>
+</div>
 </template>
---
+++ src/layout/ContactBanner.vue
@@ -0,0 +1,42 @@
+﻿<script
+	lang='ts'
+	setup
+>
+import { onBeforeMount, ref } from 'vue';
+import { getLibraryHours, readJson } from '@/utilities.ts';
+import type { ContactInfo, EventTime } from '@/types.ts';
+
+const todaysHours = ref<EventTime|null>();
+const contactInfo = ref<ContactInfo>();
+
+onBeforeMount(async () => {
+	const hours = await getLibraryHours();
+	const today = new Date().getDay();
+	todaysHours.value = hours[today];
+	contactInfo.value = await readJson<ContactInfo>('libraryContact')!;
+});
+</script>
+
+<template>
+<div class='bg-dark text-white'>
+	<div class='container d-md-flex justify-content-md-between text-center'>
+		<p class='text-light mb-0 py-2'>
+			<Icon icon='clock'/>
+			<template v-if='todaysHours'>
+				Today's hours: <Timespan :time='todaysHours'/>
+			</template>
+			<template v-else>
+				Today's hours: CLOSED
+			</template>
+		</p>
+
+		<a
+			class='d-block text-decoration-none bg-primary text-white py-2 px-3'
+			:href='`tel:${contactInfo?.phone}`'
+		>
+			<Icon icon='phone'/>
+			{{ contactInfo?.phone }}
+		</a>
+	</div>
+</div>
+</template>
