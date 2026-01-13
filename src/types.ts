d0057600c4d9c604c52a60bcb8ce5ba322dde401
speegs92 <christian@levesque.dev>
2026-01-13 11:47:01 -0600

    added EventTime type for describing the start and end time for an event

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,24 @@
 ﻿/**
+ * The start and end time for an event
+ */
+export type EventTime = {
+	/**
+	 * The time the event starts
+	 */
+	startTime: Time;
+
+	/**
+	 * The time the event ends
+	 */
+	endTime?: Time;
+
+	/**
+	 * The day of the event
+	 */
+	date?: Date;
+}
+
+/**
  * A 24-hour formatted time
  */
 export type Time = {
