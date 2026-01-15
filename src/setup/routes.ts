2c4629bb25e364cf83037267e8bb1a436b8cea55
christianlevesque <christian@levesque.dev>
2026-01-14 19:19:38 -0600

    added /contact page

--- src/setup/routes.ts
+++ src/setup/routes.ts
@@ -3,11 +3,16 @@ import type { App } from 'vue';
 import type { RouteRecordRaw } from 'vue-router';
 
 import Home from '@/views/Index.vue';
+import Contact from '@/views/Contact.vue';
 
 const routes: RouteRecordRaw[] = [
 	{
 		path: '/',
 		component: Home
+	},
+	{
+		path: '/contact',
+		component: Contact
 	}
 ];
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,27 @@
+<script
+	lang='ts'
+	setup>
+import { onBeforeMount, ref } from 'vue';
+import { readJson } from '@/utilities.ts';
+import type { ContactInformation } from '@/types.ts';
+import { ContentBlock } from '@/components';
+
+const libraryContactInfo = ref<ContactInformation>();
+
+onBeforeMount(async () => {
+	libraryContactInfo.value = await readJson<ContactInformation>('libraryContact');
+});
+</script>
+
+<template>
+<ContentBlock title='Contact us'>
+	<p>
+		Feel free to call us, fax us, email us, or come visit us in person!
+	</p>
+
+	<ContactInfo
+		v-if='libraryContactInfo'
+		:contact='libraryContactInfo'
+	/>
+</ContentBlock>
+</template>
